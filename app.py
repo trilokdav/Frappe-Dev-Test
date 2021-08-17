a20c65798b785a089fdd182b15ae69a751d66fe7
Trilok Sharma <34391589+trilokdav@users.noreply.github.com>
2021-08-18 00:16:01 +0530

    blueprint & connection established

--- app.py
+++ app.py
@@ -1,27 +1,23 @@
-from flaskext.mysql import MySQL
-from flask import Flask, render_template, session, flash
-app = Flask(__name__)
-from mysqlconnection import MySQLConnector
+from application import create_app
+from application.mysqlconnection import MySQLConnector
+
+app = create_app()
 mysql = MySQLConnector(app,'frappe')
-
-
-@app.route('/')
-def dashboard():
-    query=('''select * from frappe.books''')
-    testquery= mysql.query_db(query)
-    print(testquery)
-    return render_template('index.html',testquery=testquery)
-
-
-@app.route('/add-books')
-def add_books():
-    query=('''select * from frappe.books''')
-    testquery= mysql.query_db(query)
-    return render_template('add_books.html',testquery=testquery)
-
-
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+# from flaskext.mysql import MySQL
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ application/admin/admin_routes.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, render_template, flash, session, Flask
+
+admin = Blueprint('admin', __name__, template_folder='../assets/templates/admin', static_folder='../assets/static', url_prefix='/admin')
+
+from app import mysql
+
+
+@admin.route('/')
+def test():
+    return render_template('add_books.html')
+
+@admin.route('/add')
+def add_books():
+    query=('''select * from frappe.books''')
+    testquery= mysql.query_db(query)
+    return render_template('add_books.html',testquery=testquery)
